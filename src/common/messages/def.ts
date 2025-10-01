9acb3e5bfb04eb26685c7ac310a488357cd37c15
Chris Coulthard <chrismc16@outlook.com>
2025-10-01 01:58:40 -0700

    chore/improve-locale-framework: Actions translations now supported. Added additional translations.

--- src/common/messages/def.ts
+++ src/common/messages/def.ts
@@ -1,4 +1,5 @@
 import def from "../messagesJson/en.json";
+import action from "../locales/en/actions.json";
 import pageGettingStarted from "../locales/en/page_getting_started.json";
 import pageGeneral from "../locales/en/page_general.json";
 import pageRemote from "../locales/en/page_remote.json";
@@ -7,6 +8,7 @@ import pageMaintenance from "../locales/en/page_maintenance.json";
 import pageAbout from "../locales/en/page_about.json";
 export const PartialMessages = {
     def,
+    action,
     pageGettingStarted,
     pageGeneral,
     pageRemote,
