3d78ffa767ad0771bfb919f894b838312b2d24c9
Chris Coulthard <chrismc16@outlook.com>
2025-10-04 22:42:57 -0700

    chore/improve-locale-framework: Added dialogs.json to hold translation strings related to dialogs, including action buttons that are specific to a dialog.

--- src/common/messages/def.ts
+++ src/common/messages/def.ts
@@ -1,5 +1,6 @@
 import def from "../messagesJson/en.json";
 import action from "../locales/en/actions.json";
+import dialog from "../locales/en/dialogs.json";
 import pageGettingStarted from "../locales/en/page_getting_started.json";
 import pageGeneral from "../locales/en/page_general.json";
 import pageRemote from "../locales/en/page_remote.json";
@@ -9,6 +10,7 @@ import pageAbout from "../locales/en/page_about.json";
 export const PartialMessages = {
     def,
     action,
+    dialog,
     pageGettingStarted,
     pageGeneral,
     pageRemote,
