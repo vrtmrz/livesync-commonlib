b62c00c1d679853c0a5620a79455cf016ddfe981
vorotamoroz <vrtmrz@gmail.com>
2026-07-12 08:47:15 +0000

    refactor: split storage adapter capabilities

--- src/serviceModules/adapters/index.ts
+++ src/serviceModules/adapters/index.ts
@@ -1,6 +1,16 @@
 export type { IPathAdapter } from "./IPathAdapter.ts";
 export type { ITypeGuardAdapter } from "./ITypeGuardAdapter.ts";
 export type { IConversionAdapter } from "./IConversionAdapter.ts";
-export type { IStorageAdapter } from "./IStorageAdapter.ts";
+export type {
+    IStorageAdapter,
+    IStorageBinaryReadAccess,
+    IStorageBinaryWriteAccess,
+    IStorageDirectoryAccess,
+    IStorageMetadataAccess,
+    IStorageRemoveAccess,
+    IStorageTextAppendAccess,
+    IStorageTextReadAccess,
+    IStorageTextWriteAccess,
+} from "./IStorageAdapter.ts";
 export type { IVaultAdapter } from "./IVaultAdapter.ts";
 export type { IFileSystemAdapter } from "./IFileSystemAdapter.ts";
